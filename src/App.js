a41963952fbecc55ff85125404091ace73d1df24
lovearya <lovepreetarya1405@gmail.com>
2022-04-18 18:24:04 +0530

    todo application

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Todo from '../../todolist/src/component/todoreact/todo'
+
+
+const App = () => {
+  return (
+    <>
+      <div>
+      <Todo/>
+      </div>
+      
+    </>
+  );
+};
+
+export default App
